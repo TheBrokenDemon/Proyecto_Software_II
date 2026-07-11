e48719b88c21e2003bbf9b755e45978e298618c0
enzoc221002 <20253876@aloe.ulima.edu.pe>
2026-07-10 21:14:45 -0500

    Actualice configuracion de Vitest para reconocer tests

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'node',
-    include: ['src/**/*.vitest.ts'],
+    include: ['src/**/*.{test,vitest}.ts'],
     globals: true,
   },
 });
